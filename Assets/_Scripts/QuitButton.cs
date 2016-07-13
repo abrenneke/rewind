0e4471f015b5b378d9c95eba39c57a2d546b081c
Andy Brenneke <sneakymax@gmail.com>
2016-07-13 16:05:32 -0700

    Start screen

---
+++ Assets/_Scripts/QuitButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets._Scripts
+{
+    [UnityComponent]
+    public class QuitButton : MonoBehaviour
+    {
+        [CalledFromUnity]
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
---
+++ Assets/_Scripts/StartGameButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets._Scripts
+{
+    [UnityComponent]
+    public class StartGameButton : MonoBehaviour
+    {
+        [CalledFromUnity]
+        public void Play()
+        {
+            SceneManager.LoadScene("Game");
+        } 
+    }
+}
